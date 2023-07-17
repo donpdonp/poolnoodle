2d326cd59c7e7292a61963a98cf23a4371c93ee7
Don Park <donp@donp.org>
2023-07-17 13:06:52 -0700

    in curve, out uniswap

--- sol/contracts/twostep.sol
+++ sol/contracts/twostep.sol
@@ -2,12 +2,16 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import './interfaces/IUniswapV2Pair.sol';
+import './interfaces/ICurvePair.sol';
 
 address constant curve_steth = 0xDC24316b9AE028F1497c275EB9192a3Ea0f67022;
 address constant uniswap_steth = 0x4028DAAC072e492d34a3Afdbef0ba7e35D8b55C4;
 
 contract twostep {
-  function swap() public {
-    IUniswapV2Pair pair = IUniswapV2Pair(uniswap_steth);
+  function swap(uint out) public payable {
+    ICurvePair curve = ICurvePair(curve_steth);
+    out = curve.exchange(1, 0, msg.value, 0);
+    IUniswapV2Pair uniswap = IUniswapV2Pair(uniswap_steth);
+    uniswap.swap(out, out, msg.sender, "0x");
   }
 }
