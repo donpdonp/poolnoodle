c149c5f795e9dcde152594354a17741e9fc20128
Don Park <donp@donp.org>
2023-08-30 11:50:14 -0700

    uniswap fee calc correction

--- poolnoodle/uniswap.py
+++ poolnoodle/uniswap.py
@@ -1,19 +1,20 @@
 from decimal import Decimal
 
-#class Uniswap:
-    #def __init__(self, chain: str, contract_addr: str):
+# UniswapV2Library.sol
+# function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
+#     require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
+#     require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+#     uint amountInWithFee = amountIn.mul(997);
+#     uint numerator = amountInWithFee.mul(reserveOut);
+#     uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+#     amountOut = numerator / denominator;
+# }
 
-    # UniswapV2Library.sol
-    # function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
-    #     require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
-    #     require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
-    #     uint amountInWithFee = amountIn.mul(997);
-    #     uint numerator = amountInWithFee.mul(reserveOut);
-    #     uint denominator = reserveIn.mul(1000).add(amountInWithFee);
-    #     amountOut = numerator / denominator;
-    # }
+def getAmountOut(amountIn: Decimal, reserveIn: Decimal, reserveOut: Decimal):
+    amountInWithFee = amountIn * 997
+    numerator = amountInWithFee * reserveOut
+    denominator = (reserveIn * 1000) + amountInWithFee
+    return numerator / denominator
 
 def price(x: Decimal, y: Decimal, x_add: Decimal):
-    k = x * y
-    out = k / (x + x_add) - y
-    return -1 * x_add / out
+    return x_add / getAmountOut(x_add, x, y)
